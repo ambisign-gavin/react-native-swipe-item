9d3b3c9fe6655ef668d8cc898c9be7b7bbec3459
kuku8990 <kuku8990@gmail.com>
2019-03-13 15:46:02 +0800

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,57 @@
+declare module 'react-native-swipe-item' {
+    import { ViewStyle, ViewProps } from 'react-native';
+    import { Component, ReactChild, ReactNode } from 'react';
+
+    export class SwipeButtonsContainer extends Component<ViewProps, any> {
+    }
+
+    interface SwipeItemProps {
+        children?: ReactNode,
+        /**
+         * Swipe item layout style
+         */
+        style?: ViewStyle,
+        /**
+         * Swipe item style
+         */
+        swipeContainerStyle?: ViewStyle,
+        /**
+         * Buttons that want to show on the left when the item swiped to right.
+         */
+        leftButtons?: ReactNode,
+        /**
+         * Buttons that want to show on the right when the item swiped to right.
+         */
+        rightButtons?: ReactNode,
+        /**
+         * The component for the swipe item.
+         * More Information: https://github.com/ambisign-gavin/react-native-swipe-item#containerView
+         */
+        containerView?: ReactNode,
+        /**
+         * will be triggered when the item started swipe from the origin position
+         * @param {SwipeItem} swipeItem SwipeItem reference
+         */
+        onSwipeInitial?: (swipeItem: SwipeItem) => void,
+        /**
+         * will be triggered when left buttons showed
+         * @param {SwipeItem} swipeItem SwipeItem reference
+         */
+        onLeftButtonsShowed?: (swipeItem: SwipeItem) => void,
+        /**
+         * will be triggered when right buttons showed
+         * @param {SwipeItem} swipeItem SwipeItem reference
+         */
+        onRightButtonsShowed?: (swipeItem: SwipeItem) => void,
+        /**
+         * will be triggered when the item moved to the origin position
+         */
+        onMovedToOrigin?: (swipeItem: SwipeItem) => void,
+    }
+    export class SwipeItem extends Component<SwipeItemProps, {}> {
+        /**
+         * Close Swipe Item
+         */
+        close(): void;
+    }
+}
